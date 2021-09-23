7f8c71c4b8226dcc6d3d2a4f46fdccf280fe7efc
ikmalalfaozi <16520205@std.stei.itb.ac.id>
2021-09-24 00:33:25 +0700

    Menambahkan Input/Output, Create jenis-jenis matriks, operasi matriks dasar, dan operasi baris elementer

--- src/Matrix.java
+++ src/Matrix.java
@@ -60,9 +60,9 @@ public class Matrix {
         } else {
             col = Content.get(0).size();
             this.Content = new double[row][col];
-            this.RowEff = row + 1;
+            this.RowEff = row;
             this.ColEff = col;
-            for (int i = 0; i <= row; i ++) {
+            for (int i = 0; i < row; i ++) {
                 for (int j = 0; j < col; j ++) {
                     this.Content[i][j] = Content.get(i).get(j);
                 }
@@ -73,29 +73,40 @@ public class Matrix {
 
     /* **** Selektor **** */
     int getLastIdxRow (Matrix m) {
-        // Mengembalikan indeks terakhir baris 
-        return m.RowEff - 1;
+    // Mengembalikan indeks terakhir baris 
+    // KAMUS LOKAL
+    // ALGORITMA
+    return m.RowEff - 1;
     }
 
     int getLastIdxCol (Matrix m) {
-        // Mengembalikan indeks terakhir kolom
-        return m.ColEff - 1;
+    // Mengembalikan indeks terakhir kolom
+    // KAMUS LOKAL
+    // ALGORITMA
+    return m.ColEff - 1;
     }
 
     int getNumberOfEelement (Matrix m) {
-        // Mengembalikan ukuran matriks
+    // Mengembalikan ukuran matriks
+    // KAMUS LOKAL
+    // ALGORITMA
         return m.RowEff * m.ColEff;
     }
 
     /* **** Predikat **** */
     boolean isSquare (Matrix m) {
-        // Mengembalikan true jika matriks tersebut adalah matriks bujur sangkar
+    // Mengembalikan true jika matriks tersebut adalah matriks bujur sangkar
+    // KAMUS LOKAL
+    // ALGORITMA
         return (m.RowEff == m.ColEff);
     }
+
     boolean isIdentity (Matrix m) {
-        // Mengembalikan true jika matriks tersebut adalah matriks identitas 
+    // Mengembalikan true jika matriks tersebut adalah matriks identitas 
+    // KAMUS LOKAL
         int i = 0;
         int j;
+    // ALGORITMA
         boolean flag = true;
 
         if (isSquare(m)) {
@@ -123,5 +134,157 @@ public class Matrix {
             flag = false;
         }
         return flag;
+    }
+
+    /* **** Create Jenis-Jenis Matriks **** */
+    Matrix createMatrixIdentitas(int N) {
+    // Membuat matriks identitas
+    // KAMUS LOKAL
+        int i,j;
+        Matrix I = new Matrix(N,N);
+    // ALGORITMA
+        for (i = 0; i < I.ColEff; i++) {
+            for (j = 0; j < I.RowEff; j++) {
+                if (i == j) {
+                    I.Content[i][j] = 1;
+                } else {
+                    I.Content[i][j] = 0;
+                }
+            }
+        }
+        return I;
+    }
+
+    Matrix createMatrixHilbert(int N) {
+    // Membuat matriks Hilbert
+    // KAMUS LOKAL
+        int i,j;
+        Matrix H = new Matrix(N,N);
+    // ALGORITMA
+        for (i = 0; i < H.RowEff; i++) {
+            for (j = 0; j < H.ColEff; j++) {
+                H.Content[i][j] = 1.0/(i+j+1);
+            }
+        }
+        return H;
+    }
+    
+    /* **** Input Output Matriks **** */
+    void readMatrix() {
+    // Membaca inputan matriks dari keyboard
+    // KAMUS LOKAL
+        int i, j;
+    // ALGORITMA
+        System.out.println("Masukkan elemen matriks: ");
+        for (i = 0; i < this.RowEff; i++) {
+            for (j = 0; j < this.ColEff; j++) {
+                this.Content[i][j] = input.nextDouble();
+            }
+        }
+
+    }
+
+    void displayMatrix() {
+    // Menampilkan isi matriks
+    // KAMUS LOKAL
+        int i, j;
+    // ALGORITMA
+        for (i = 0; i < this.RowEff; i++) {
+            for (j = 0; j < this.ColEff; j++) {
+                System.out.printf("%.2f ",this.Content[i][j]);
+            } 
+            System.out.print("\n");
+        }
+    }
+    
+
+    /* **** Operasi-Operasi Matriks Dasar **** */
+    Matrix multiplyMatrix (Matrix m1, Matrix m2) {
+    // Mengalikan matriks m1 dengan m2
+    // KAMUS LOKAL
+        int i, j;
+        Matrix mHasil = new Matrix(m1.RowEff, m2.ColEff);
+    // ALGORITMA
+        for (i = 0; i < m1.RowEff; i++) {
+            for (j = 0; j < m2.ColEff; j++) {
+                mHasil.Content[i][j] = 0;
+                for (int k = 0; k < mHasil.ColEff; k++) {
+                    mHasil.Content[i][j] += m1.Content[i][k] * m2.Content[k][j];
+                }
+            }
+        }
+        return mHasil;
+    }
+
+    Matrix multiplyConst (Matrix m1, float k) {
+    // Mengalikan matriks m1 dengan k
+    // KAMUS LOKAL
+        Matrix mHasil = new Matrix(m1.RowEff,m1.ColEff);
+        int i, j;
+    // ALGORITM
+        for (i = 0; i < m1.RowEff; i++) {
+            for (j = 0; j < m1.ColEff; j++) {
+                mHasil.Content[i][j] = m1.Content[i][j]*k;
+            }
+        }
+        return mHasil;
+    }
+    
+    Matrix addMatrix(Matrix m1, Matrix m2) {
+    // KAMUS LOKAL menambah matrix m1 dengan m2
+        int i, j;
+        Matrix mHasil = new Matrix(m1.RowEff,m1.ColEff);
+    // ALGORITMA
+        for (i = 0; i < mHasil.RowEff; i++) {
+            for (j = 0; j < mHasil.ColEff; j++) {
+                mHasil.Content[i][j] = m1.Content[i][j] + m2.Content[i][j];
+            }
+
+        }
+        return mHasil;
+    }
+    
+    Matrix subtractMatrix(Matrix m1, Matrix m2) {
+    // Mengurangi matrix m1 dengan m2
+    // KAMUS LOKAL
+        int i,j;
+        Matrix mHasil = new Matrix(m1.RowEff,m1.ColEff);
+    // ALGORITMA
+        for (i = 0; i < mHasil.RowEff; i++) {
+            for (j = 0; j < mHasil.ColEff; j++) {
+                mHasil.Content[i][j] = m1.Content[i][j] - m1.Content[i][j];
+            }
+        }
+        return mHasil;
+    }
+    
+    /* **** Operasi-Operasi Baris Elementer **** */
+    void swapRow(int row1, int row2) {
+    // Menukar baris
+    // KAMUS LOKAL
+        double[] temp = Content[row1];
+    // ALGORITMA
+        Content[row1] = Content[row2];
+        Content[row2] = temp;
+    }
+
+    void multiplyRow(int row, double k) {
+    // Mengalikan baris (row) dengan k
+    // KAMUS LOKAL
+        int j;
+    // ALGORITMA
+        for (j = 0; j < this.ColEff; j++) {
+            Content[row][j] *= k;
+        }
+    }
+
+    void plusRow(int row1, int row2, double k) {
+    // Menambah baris 1 (row1) dengan k kali baris 2 (row2) 
+    // KAMUS LOKAL
+        int j;
+    // ALGORITMA
+        for (j = 0; j < this.ColEff; j++) {
+            Content[row1][j] += Content[row2][j]*k;
+        }
     }
 }
