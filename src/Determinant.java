35bf77a9d8d0b575ff5c834f7e40402db9be3575
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2021-09-27 23:35:14 +0700

    add determinant class & OBE method

--- src/Determinant.java
+++ src/Determinant.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
 public class Determinant {
-    
+    Scanner input = new Scanner(System.in);
+
+    public void OBEMethod(Matrix m) {
+        System.out.print("Masukan jumlah baris matriks: ");
+        int M = input.nextInt();
+        System.out.print("Masukan jumlah kolom matriks: ");
+        int N = input.nextInt();
+        
+        m.RowEff = M;
+        m.ColEff = N;
+        m.readMatrix();
+
+        if (m.isSquare(m)) {
+            System.out.println("Determinan matriks adalah " + m.determinantOBE(m));
+        } else {
+            System.out.println("Matriks bukan matriks persegi.");
+        }
+    }
 }
