ced6a90ca832af7d95f57470a8677dbc30e50e33
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2021-09-29 22:22:25 +0700

    tambah output ke file pada class SPL

--- src/SPL.java
+++ src/SPL.java
@@ -1,7 +1,7 @@
 
 import java.io.FileNotFoundException;
 import java.util.*;
-
+import java.io.FileWriter;
 import javax.xml.crypto.dsig.XMLObject;
 
 public class SPL {
@@ -16,7 +16,23 @@ public class SPL {
         }
     }
 
-    public static ArrayList<String> returnSolution(Matrix m) {
+    public static void printSolution(ArrayList<String> s, String fileOut) {
+        // KAMUS LOKAL
+        int i;
+        // ALGORITMA
+        try {
+            FileWriter writer = new FileWriter(fileOut);
+            for (i = 0; i < s.size(); i ++) {
+                writer.write(s.get(i));
+            }
+            writer.close();
+            System.out.println("Jawaban berhasil ditulis ke dalam file.");
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+    }
+
+    public static ArrayList<String> returnSolutionGauss(Matrix m) {
         // KAMUS LOKAL
         int i, j, k, countRow0; 
         int count0 = 0;
@@ -121,11 +137,106 @@ public class SPL {
         return solution;
     }
 
+    public static ArrayList<String> returnSolutionGaussJordan(Matrix m) {
+        // KAMUS LOKAL
+        int i, j, k, countRow0; 
+        int count0 = 0;
+        ArrayList<String> solution;
+        boolean found;
+
+        // ALGORITMA
+        solution = new ArrayList<String>();
+        i = 0;
+        boolean flag = true;
+        while (i < m.RowEff && flag) {
+            count0 = 0;
+            j = 0;
+            while (j < m.ColEff - 1 && flag) {
+                if (m.Content[i][j] == 0) {
+                    count0 += 1;
+                }
+                if (count0 == m.ColEff - 1 && j == m.ColEff - 2) {
+                    if (m.Content[i][j+1] != 0) {
+                        flag = false;
+                    } else {
+                        count0 += 1;
+                        flag = false;
+                    }
+                } else {
+                    j++;
+                }
+            }
+            if (flag) {
+                i ++;
+            }
+        }
+
+        if (count0 ==  m.ColEff - 1) {
+            solution.add("SPL tidak mempunyai solusi");
+        } else if (count0 == m.ColEff) {
+            k = 0;
+            for (i = m.RowEff-1; i >= 0; i --) {
+                countRow0 = 0;
+                found = false;
+                j = 0;
+                while (j < m.ColEff && !found) {
+                    if (m.Content[i][j] != 0) {
+                        found = true;
+                    } else {
+                        j++;
+                        countRow0 += 1;
+                    }
+                }
+
+                int NextCountRow0 = countRow0;
+                int col;
+                for (col = j+1; col < m.ColEff; col++) {
+                    if (m.Content[i][col] == 0) {
+                        NextCountRow0 += 1;
+                    }
+                } 
+
+                //System.out.println(countRow0);
+                if (countRow0 == m.ColEff) {
+                    k += 1;
+                    solution.add("x" + (i + 1) + " = " + "r" + k);
+                    //System.out.println(solution);
+                } else if (NextCountRow0 == m.ColEff - 1) {
+                    solution.add("x" + (i + 1) + " = " + 0);
+                } else {
+                    String temp = "";
+                    if (m.Content[i][m.ColEff-1] != 0) {
+                        temp += String.valueOf(m.Content[i][m.ColEff-1]);
+                    }
+                    for (int kol = j+1; kol < m.ColEff-1; kol ++) {
+                        //System.out.println(kol);
+                        if (m.Content[i][kol] != 0) {
+                            temp += " + " + (-m.Content[i][kol]) + "r" + (m.ColEff - 1 - kol) ;
+                            System.out.println(temp);
+                        }
+                        // GAUSS
+                          /*1 1 2 4
+                           0 1 1 2
+                           0 0 0 0 */
+                    }
+                    solution.add("x" + (i + 1) + " = " + temp);
+                    //System.out.println(solution); 
+                } 
+            } 
+        } else {
+            for (i = m.RowEff-1; i >= 0; i --) {
+                String temp2 = "x" + (i + 1) + " = " + m.Content[i][m.ColEff-1];
+                solution.add(temp2);
+            }
+        }
+        return solution;
+    }
+
     public static void gaussMethod(Matrix m) {
         // KAMUS LOKAL
 
         // ALGORITMA
-        
+
         m.gaussElimination(m);
     }
 
@@ -142,14 +253,6 @@ public class SPL {
         // KAMUS LOKAL
 
         // ALGORITMA
-        System.out.print("Masukan jumlah baris matriks: ");
-        int M = input.nextInt();
-        System.out.print("Masukan jumlah kolom matriks: ");
-        int N = input.nextInt();
-        
-        m.RowEff = M;
-        m.ColEff = N;
-        m.readMatrix();
         m.gaussJordanElimination(m);
     }
 
@@ -196,6 +299,48 @@ public class SPL {
         }
     }
 
+    public static void inversMethod(Matrix m, String fileOut) {
+        // Memecahkan SPL dengan metode matriks balikan
+        // KAMUS LOKAL
+        Matrix minv, mcons, mhasil;
+        int i, j;
+
+        // ALGORITMA
+        
+        minv = new Matrix(m.RowEff, m.ColEff-1);
+
+        for (i = 0; i < minv.RowEff; i ++) {
+            for (j = 0; j < minv.ColEff; j ++) {
+                minv.Content[i][j] = m.Content[i][j];
+            }
+        }
+
+        try {
+            FileWriter writer = new FileWriter(fileOut);
+            if (minv.isSquare(minv) && minv.determinantCofactor(minv) != 0) {
+                minv = minv.inverseCofaktor(minv);
+                mcons = new Matrix(m.RowEff, 1);
+                for (i = 0; i < m.RowEff; i ++) {
+                    mcons.Content[i][0] = m.Content[i][m.ColEff-1];
+                }
+                mhasil = new Matrix(m.RowEff, 1);
+                mhasil = minv.multiplyMatrix(minv, mcons);
+                for (i = 0; i < mhasil.RowEff; i ++) {
+                    for (j = 0; j < mhasil.ColEff; j ++) {
+                        writer.write(String.valueOf("x" + (i + 1) + " = " + mhasil.Content[i][j]));
+                    }
+                }
+                writer.close();
+            } else {
+                System.out.println("Matriks tidak mempunyai invers. Keterangan berhasil ditulis ke dalam file.");
+                writer.write("Matriks tidak mempunyai invers.");
+                writer.close();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public void inversMethod(String fileName) throws FileNotFoundException {
         // Memecahkan SPL dengan metode matriks balikan
         // KAMUS LOKAL
@@ -231,6 +376,49 @@ public class SPL {
         }
     }
 
+    public static void inversMethod(String fileName, String fileOut) throws FileNotFoundException {
+        // Memecahkan SPL dengan metode matriks balikan
+        // KAMUS LOKAL
+        Matrix m, minv, mcons, mhasil;
+        int i, j;
+
+        // ALGORITMA
+        m = new Matrix(fileName);
+        
+        minv = new Matrix(m.RowEff, m.ColEff-1);
+
+        for (i = 0; i < minv.RowEff; i ++) {
+            for (j = 0; j < minv.ColEff; j ++) {
+                minv.Content[i][j] = m.Content[i][j];
+            }
+        }
+
+        try {
+            FileWriter writer = new FileWriter(fileOut);
+            if (minv.isSquare(minv) && minv.determinantCofactor(minv) != 0) {
+                minv = minv.inverseCofaktor(minv);
+                mcons = new Matrix(m.RowEff, 1);
+                for (i = 0; i < m.RowEff; i ++) {
+                    mcons.Content[i][0] = m.Content[i][m.ColEff-1];
+                }
+                mhasil = new Matrix(m.RowEff, 1);
+                mhasil = minv.multiplyMatrix(minv, mcons);
+                for (i = 0; i < mhasil.RowEff; i ++) {
+                    for (j = 0; j < mhasil.ColEff; j ++) {
+                        writer.write(String.valueOf("x" + (i + 1) + " = " + mhasil.Content[i][j]));
+                    }
+                }
+                writer.close();
+            } else {
+                System.out.println("Matriks tidak mempunyai invers. Keterangan berhasil ditulis ke dalam file.");
+                writer.write("Matriks tidak mempunyai invers.");
+                writer.close();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public static void cramerMethod(Matrix m) {
         // KAMUS LOKAL 
         ArrayList<Double> arrayHasil;
@@ -258,6 +446,41 @@ public class SPL {
         }
         
 
+    }   
+
+    public static void cramerMethod(Matrix m, String fileOut) {
+        // KAMUS LOKAL 
+        ArrayList<Double> arrayHasil;
+        int i, k, l;
+        Matrix mTest;
+
+        // ALGORITMA
+
+        mTest = new Matrix(m.RowEff, m.ColEff - 1);
+        arrayHasil = new ArrayList<Double>();
+
+        for (k = 0; k < mTest.RowEff; k ++) {
+            for (l = 0; l < mTest.ColEff; l ++) {
+                mTest.Content[k][l] = m.Content[k][l];
+            }
+        }
+
+        try {
+            FileWriter writer = new FileWriter(fileOut);
+            if (mTest.isSquare(mTest) && mTest.determinantCofactor(mTest) != 0) {
+                arrayHasil = m.cramerRule(m);
+                for (i = 0; i < arrayHasil.size(); i ++) {
+                    writer.write(String.valueOf("x" + (i + 1) + " = " + arrayHasil.get(i)));
+                }
+                writer.close();
+            } else {
+                System.out.println("Determinan matriks sama dengan nol. Tidak bisa menggunakan metode cramer.\nKeterangan berhasil ditulis ke dalam file.");
+                writer.write("Determinan matriks sama dengan nol. Tidak bisa menggunakan metode cramer.");
+                writer.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }   
 
     public void cramerMethod(String fileName) throws FileNotFoundException {
@@ -289,10 +512,38 @@ public class SPL {
 
     }   
 
-    public void tulisSPL(Matrix m) {
-        // KAMUS LOKAL
+    public static void cramerMethod(String fileName, String fileOut) throws FileNotFoundException {
+        // KAMUS LOKAL 
+        ArrayList<Double> arrayHasil;
+        int i, k, l;
+        Matrix m, mTest;
 
-        // ALGORITMA 
+        // ALGORITMA
+        m = new Matrix(fileName);
+        mTest = new Matrix(m.RowEff, m.ColEff - 1);
+        arrayHasil = new ArrayList<Double>();
 
-    }
+        for (k = 0; k < mTest.RowEff; k ++) {
+            for (l = 0; l < mTest.ColEff; l ++) {
+                mTest.Content[k][l] = m.Content[k][l];
+            }
+        }
+
+        try {
+            FileWriter writer = new FileWriter(fileOut);
+            if (mTest.isSquare(mTest) && mTest.determinantCofactor(mTest) != 0) {
+                arrayHasil = m.cramerRule(m);
+                for (i = 0; i < arrayHasil.size(); i ++) {
+                    writer.write(String.valueOf("x" + (i + 1) + " = " + arrayHasil.get(i)));
+                }
+                writer.close();
+            } else {
+                System.out.println("Determinan matriks sama dengan nol. Tidak bisa menggunakan metode cramer.\nKeterangan berhasil ditulis ke dalam file.");
+                writer.write("Determinan matriks sama dengan nol. Tidak bisa menggunakan metode cramer.");
+                writer.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }   
 }
