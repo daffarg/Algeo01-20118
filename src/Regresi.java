28299f1d19e48d3c8fa8be04c4b73f36a47ccef2
ikmalalfaozi <16520205@std.stei.itb.ac.id>
2021-09-30 22:46:23 +0700

    Menambahkan method readRegresi di class Regresi

--- src/Regresi.java
+++ src/Regresi.java
@@ -51,26 +51,97 @@ public class Regresi {
             }
         }
 
-        // Mengisi kolom pertama dari baris kedua
-
-        for (i = 1; i < m.RowEff; i ++) {
-            m.Content[i][0] = 0;
-            for (k = 0; k < numOfData; k ++) {
-                m.Content[i][0] += data.get(k).get(i - 1);
-            }
-        }
-
-        // Mengisi elemen matriks sisanya
-
-        for (i = 1; i < m.RowEff; i ++) {
-            for (j = 1; j < m.ColEff; j ++) {
+        // Mengisi matriks baris berikutnya
+        for (i = 1; i < m.RowEff; i++) {
+            for (j = 0; j < m.ColEff; j++) {
                 m.Content[i][j] = 0;
-                for (k = 0; k < numOfData; k ++) {
-                    m.Content[i][j] += data.get(k).get(i) * data.get(k).get(i - 1);
+                if (j == 0) {
+                    for (k = 0; k < numOfData; k++) {
+                        m.Content[i][j] += data.get(k).get(i-1);
+                    }
+                } else {
+                    for (k = 0; k < numOfData; k++) {
+                        m.Content[i][j] += data.get(k).get(j-1)*data.get(k).get(i-1);
+                    }
                 }
             }
         }
 
+        // // Mengisi kolom pertama dari baris kedua
 
+        // for (i = 1; i < m.RowEff; i ++) {
+        //     m.Content[i][0] = 0;
+        //     for (k = 0; k < numOfData; k ++) {
+        //         m.Content[i][0] += data.get(k).get(i - 1);
+        //     }
+        // }
+
+        // // Mengisi elemen matriks sisanya
+
+        // for (i = 1; i < m.RowEff; i ++) {
+        //     for (j = 1; j < m.ColEff; j ++) {
+        //         m.Content[i][j] = 0;
+        //         for (k = 0; k < numOfData; k ++) {
+        //             m.Content[i][j] += data.get(k).get(i) * data.get(k).get(i - 1);
+        //         }
+        //     }
+        // }
+    }
+
+    public static void readRegresi(Matrix m, String fileName) throws FileNotFoundException {
+        // KAMUS LOKAL
+        int i, j, k;
+        Matrix mData;
+
+        mData = new Matrix(fileName);
+
+
+        // Mengisi mariks baris pertama
+        for (j = 0; j < m.ColEff; j ++) {
+            if (j == 0) {
+                m.Content[0][j] = mData.RowEff;
+            } else {
+                m.Content[0][j] = 0;
+                for (k = 0; k < mData.RowEff; k ++) {
+                    m.Content[0][j] += mData.Content[k][j - 1];
+                }
+            }
+        }
+
+        // Mengisi matriks baris berikutnya
+        for (i = 1; i < m.RowEff; i++) {
+            for (j = 0; j < m.ColEff; j++) {
+                m.Content[i][j] = 0;
+                if (j == 0) {
+                    for (k = 0; k < mData.RowEff; k++) {
+                        m.Content[i][j] += mData.Content[k][i-1];
+                    }
+                } else {
+                    for (k = 0; k < mData.RowEff; k++) {
+                        m.Content[i][j] += mData.Content[k][j-1]*mData.Content[k][i-1];
+                    }
+                }
+            }
+        }
+
+        // // Mengisi kolom pertama dari baris kedua
+
+        // for (i = 1; i < m.RowEff; i ++) {
+        //     m.Content[i][0] = 0;
+        //     for (k = 0; k < numOfData; k ++) {
+        //         m.Content[i][0] += data.get(k).get(i - 1);
+        //     }
+        // }
+
+        // // Mengisi elemen matriks sisanya
+
+        // for (i = 1; i < m.RowEff; i ++) {
+        //     for (j = 1; j < m.ColEff; j ++) {
+        //         m.Content[i][j] = 0;
+        //         for (k = 0; k < numOfData; k ++) {
+        //             m.Content[i][j] += data.get(k).get(i) * data.get(k).get(i - 1);
+        //         }
+        //     }
+        // }
     }
 }
